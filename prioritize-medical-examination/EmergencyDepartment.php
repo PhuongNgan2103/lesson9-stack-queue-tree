0fb919b15f9b8b41f995cf24d6d1efa10e57a2f3
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2020-12-24 10:03:08 +0700

    lesson9-stack-queue-tree

--- prioritize-medical-examination/EmergencyDepartment.php
+++ prioritize-medical-examination/EmergencyDepartment.php
@@ -3,35 +3,49 @@ include_once "patient.php";
 
 class EmergencyDepartment
 {
-   protected $patients = [];
+
+    public $patients;
+
+
+    public function __construct()
+    {
+        $this->patients = new SplDoublyLinkedList();
+    }
 
     public function isEmpty()
     {
-        return empty($this->patients);
-   }
+        return ($this->patients == null);
+
+    }
 
     public function add($patient)
     {
-        if (!$this->isEmpty()){
-            foreach ($this->patients as $key=>$value){
-                if ($value < $patient->code){
+        if (!$this->isEmpty()) {
+            foreach ($this->patients as $key => $value) {
+                if ($value->code < $patient->code) {
                     continue;
                 }
-                array_splice($this->patients,$key,0,$patient);
+                $this->patients->add($key, $patient);
                 return;
             }
-            $this->patients[] = $patient;
+            $this->patients->push($patient);
+        } else {
+            $this->patients->push($patient);
         }
-       else{
-           $this->patients[] = $patient;
-       }
-   }
+    }
 
-    public function get()
+    public function display()
     {
-        $patient = $this->patients[0];
-        unset($this->patients[0]);
+        foreach ($this->patients as $key => $patient) {
+            echo $key . '-' .$patient->name .'-'.$patient->code."<br>";
+        }
+    }
+
+    public function examination()
+    {
+        $patient = $this->patients->shift();
+        echo "Đã đưa".'-'.$patient->name.'-'. "đi khám";
         return $patient;
-   }
+    }
 
 }
--- prioritize-medical-examination/index.php
+++ prioritize-medical-examination/index.php
@@ -2,17 +2,18 @@
 include_once "EmergencyDepartment.php";
 include_once "patient.php";
 
-$ed = new EmergencyDepartment();
+$ed =  new EmergencyDepartment();
 
-echo "<pre>";
-for ($i = 0 ; $i<6 ; $i++){
-    $patient = new Patient("benh nhan",$i);
+$name =["minh","thuong","an","khoi"];
+
+for ($i =1;$i<6;$i++){
+    $rand = rand(1,6);
+    $nameIndex = rand(0,count($name)-1);
+    $patient = new Patient($name[$nameIndex],$rand);
     $ed->add($patient);
-    var_dump($patient);
 }
-
-$patient = $ed->get();
-
-echo $patient->name;
 echo "<pre>";
-echo $patient->code;
+$ed->display();
+$ed->examination();
+echo "<br>";
+$ed->display();
